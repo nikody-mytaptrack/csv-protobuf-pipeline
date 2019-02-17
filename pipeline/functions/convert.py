a493a5923405ce3b0cd1062790aba8a4d77657ff
Nikody Keating <nikody.keating@gmail.com>
2019-02-17 17:52:25 -0500

    Adding update to pass tags from input objects to output objects

--- pipeline/functions/convert.py
+++ pipeline/functions/convert.py
@@ -9,6 +9,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 s3 = boto3.resource('s3')
+client = boto3.client('s3')
 serializer = numpy_to_record_serializer()
 
 def toProto(event, context):
@@ -18,14 +19,38 @@ def toProto(event, context):
         bucket = s3Record['bucket']['name']
         key = s3Record['object']['key']
         
+        logger.info('Getting original object')
+        origin = s3.Object(bucket, key)
+        logger.info('Object retrieved')
+        logger.info(origin)
+
+        logger.info('Getting tags')
+        tagging = client.get_object_tagging(Bucket=bucket,Key=key)
+        logger.info('Tags retrieved')
+        logger.info(tagging['TagSet'])
+        
+        tags = ''
+        for tag in tagging['TagSet']:
+            tags += tag['Key'] + '=' + tag['Value'] + ','
+
+        tags = tags[0: len(tags) - 1]
+
+        logger.info('Downloading file')
         localFile = '/tmp/' + str(key).replace('/', '-')
-        s3.Bucket(bucket).download_file(key, localFile)
+        origin.download_file(localFile)
+        
+        logger.info('Converting with pandas')
         data = pandas.read_csv(localFile, delimiter=',')
         ndarray = data.values
         buffer = serializer(ndarray)
+        
+        logger.info('Uploading file')
         outKey = os.environ['train_data_prefix'] + key
         outBucket = os.environ['train_data_bucket']
-        s3.Bucket(outBucket).Object(outKey).upload_fileobj(buffer)
+        s3Object = s3.Object(outBucket, outKey)
+        s3Object.put(Body=buffer, Tagging=tags)
+
+        logger.info('Deleting old file');
         s3.Object(bucket, key).delete()
     except:
         logger.error(sys.exc_info()[0])
